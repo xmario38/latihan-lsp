6d0b9f84a0817e3b4f92217f7745ed23c8577a10
Dylan <54340284+xmario38@users.noreply.github.com>
2022-03-24 11:28:22 +0700

    Add files via upload

---
+++ toko_elektronik/layout/navbar.php
@@ -0,0 +1,64 @@
+<?php
+
+
+require 'functions.php';
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Customer</title>
+    <link rel="stylesheet" href="style/index.css">
+</head>
+<body>
+
+    <div class="navbar">
+        <div class="nav-logo">
+            <a href="index.php">PRINTER DIESEL</a>
+        </div>
+        <div class="nav-links">
+            <ul>
+                <li>
+                    <a href="index.php">Beranda</a>
+                </li>
+                <li>
+                    <a href="cart.php">Keranjang Belanja</a>
+                </li>
+                <li>
+                    <a href="dashboard.php">Dashboard</a>
+                </li>
+                
+                <?php if(isset($_SESSION["username"])) : ?>     
+                    <li>
+                        <a href="#">
+                            Halo, <?= $_SESSION["name"]; ?>
+                        </a>    
+                    </li>
+                    <li>   
+                        <a href="logout.php">Logout</a>
+                    </li>
+                
+            <?php endif; ?>
+            <?php if(!isset($_SESSION["username"])) : ?>
+                
+                    <li><a href="login/index.php">Login</a></li>
+                    <li><a href="register/index.php">Register</a></li>
+            </ul>
+            <?php endif; ?>
+        </div>
+    </div>
+
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+  AOS.init();
+</script>
+</body>
+</html>
---
+++ toko_elektronik/layout/sidebar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halamn Admin</title>
+    <link rel="stylesheet" href="../style/admin.css">
+</head>
+<body>
+    
+    <div class="sidebar">
+        <a href="index.php">Dashboard</a>
+        <a href="product.php">Produk</a>
+        <a href="transaksi.php">Transaksi</a>
+        <a href="verifikasi_transaksi.php">Verifikasi Transaksi</a>
+        <a href="../logout.php">Logout</a>
+    </div>
+
+</body>
+</html>
